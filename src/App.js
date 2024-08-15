a32f5625d1e75f83c18a3dc0e2213c2c7b054857
Abdul Rehman <abdulrehmang571@gmail.com>
2024-08-15 12:11:55 +0500

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import Menu from './Components/Menu';
 import CV from './Components/CV';
 
 const App = () => {
     return (
         <div>
-            <Menu />
             <CV />
         </div>
     );
 };
-
-/*
-
-Menu
-- AddSection
-- Reorganize
-- ChnageTemplate
-CV
-- Section 1 // personal info
-- Section 2 // projects
-- Section 3 // skills
-- Section 4 // education
-
- */
 
 export default App;
